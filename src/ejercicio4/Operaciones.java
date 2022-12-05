288a1039cf4b24b481c8d735e0643a984a75afe7
Antonio Navarro <antonio.navarro@iesnervion.es>
2022-12-05 08:48:33 +0100

    La clase ya es pública

--- src/ejercicio4/Operaciones.java
+++ src/ejercicio4/Operaciones.java
@@ -4,26 +4,26 @@ package ejercicio4;
 
 public class Operaciones {
     // ^ Definimos las variables num1 y num2
-    public double num1;
-    public double num2;
+    double num1;
+    double num2;
     
     // ^ Definimos el método suma
-    public double suma() {
+    double suma() {
         return num1 + num2;
     }
 
     // ^ Definimos el método resta
-    public double resta() {
+    double resta() {
         return num1 - num2;
     }
 
     // ^ Definimos el método multiplicación
-    public double multiplicacion() {
+    double multiplicacion() {
         return num1 * num2;
     }
     
     // ^ Definimos el método división
-    public double division() {
+    double division() {
         return num1 / num2;
     }
 }
